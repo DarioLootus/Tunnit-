cbe1cfbeb83d9320f7cbdb084e1be0b0812df3c0
Opilane TTHK <150146709+DarioLootus@users.noreply.github.com>
2024-02-23 11:37:21 +0200

    ree
    -

---
+++ ListFor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Listfor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeNumbers = new List<int>();
+            primeNumbers.Add(2);
+            primeNumbers.Add(3);
+            primeNumbers.Add(5);
+            primeNumbers.Add(7);
+
+            Console.WriteLine("No of elements:" + primeNumbers.Count);
+        }
+    }
+}
